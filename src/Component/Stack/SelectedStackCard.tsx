336ba56e90a13632ccfe0439244039c8a147ee99
Fahim-mukim <fahim.u.mukim@gmail.com>
2026-09-13 14:31:42 +0600

    removing item from your stack functionalities applied

--- src/Component/Stack/SelectedStackCard.tsx
+++ src/Component/Stack/SelectedStackCard.tsx
@@ -1,24 +1,33 @@
+import { CiSquareRemove } from "react-icons/ci";
 import type IStack from "../../type/stack";
 
-const SelectedStackCard = ({ stack , onRemove}: { stack: IStack ; onRemove:(id:number) => void;}) => {
+const SelectedStackCard = ({ stack, onRemove }: { stack: IStack; onRemove: (id: number) => void; }) => {
     return (
-        <div className="  p-1.5  flex items-center gap-3">
-            <img
-                src={stack.logo}
-                alt={stack.name}
-                className="h-5 w-5 "
-            />
+        <div className=" flex items-center p-2 justify-between">
+            <div className="  p-1.5  flex  items-center gap-3">
+                <img
+                    src={stack.logo}
+                    alt={stack.name}
+                    className="h-5 w-5 "
+                />
 
-            <div>
-                <h3 className="font-medium">
-                    {stack.name}
-                </h3>
+                <div>
+                    <h3 className="font-medium">
+                        {stack.name}
+                    </h3>
 
-                <p className="text-xs text-gray-500">
-                    {stack.useFor}
-                </p>
+                    <p className="text-xs text-gray-500">
+                        {stack.useFor}
+                    </p>
+                </div>
+
+            </div>
+            <div >
+
+                <CiSquareRemove className="h-5 w-5" onClick={() => onRemove(stack.id)} />
             </div>
         </div>
+
     );
 };
 
